9f5b4070e2815517d555a40d65b6989e136251fb
colinbernard <colinjbernard@hotmail.com>
2018-10-16 20:00:23 -0700

    Many major changes. Improved overall graphics. Added start screen. Improved CSS to allow a smaller iframe size. Added more sounds.

--- index.php
+++ index.php
@@ -18,14 +18,20 @@
 <body onload="init('<?=isset($_GET['title'])?$_GET['title']:null?>');"><!-- body onload calls function to initialize game -->
 	<div class="container">
 		<div class="row" id="heading-bar">
-			<div class="col-md-3">
-				<img id="logo" height="50px" src="images/logo.png">
+			<div class="col-sm-2">
+				<img id="logo" src="images/logo.png">
 			</div>
-			<div class="col-md-6 text-center">
-				<h2 id="game-title">Test Title</h2>
+			<div class="col-sm-8 text-center">
+				<h2 id="game-title"></h2>
 			</div>
-			<div class="col-md-3">
+			<div class="col-sm-2">
 				<a onclick="toggleMute()"><img id="mute-btn" src="images/unmute.png"></a>
+			</div>
+		</div>
+		<div class="row text-center" id="start-row">
+			<div class="col-md-12">
+				<p id="start-text">The following multiple choice quiz will describe a triangle as well as show you a picture. You will need to select the correct type of triangle from the four options.</p>
+				<button id="start" onclick="start()">Start</button>
 			</div>
 		</div>
 		<div class="row text-center" id="game-over-row">
@@ -33,36 +39,37 @@
 				<p id="quiz-complete">Quiz Complete!</p>
 				<p id="score-text">Score: 0/0</p>
 				<p id="total-time">Total Time: 0 seconds</p>
-				<button onclick="restart()">Restart</button>
+				<button id="restart" onclick="restart()">Restart</button>
 			</div>
 		</div>
 		<div class="row text-center" id="question-row">
-			<div class="col-md-6 question">
-				<p id="question-text">This triangle has three equal sides. It is:</p>
+			<div class="col-sm-6 question">
+				<p id="question-text"></p>
 			</div>
-			<div class="col-md-6 question">
+			<div class="col-sm-6 question">
 				<img id="question-image" src="images/triangle.png">
 			</div>
 		</div>
 		<div class="row">
-			<div class="col-md-12">
+			<div class="col-md-12" id="form-div">
 				<form action="">
-				  <input type="radio" name="answer" value="1" class="option" id="option1"><label for="option1" class="option"> an equilateral triangle</label><br>
-				  <input type="radio" name="answer" value="2" class="option" id="option2"><label for="option2" class="option"> an isosceles triangle</label><br>
-				  <input type="radio" name="answer" value="3" class="option" id="option3"><label for="option3" class="option"> a scalene triangle</label><br>
-				  <input type="radio" name="answer" value="4" class="option" id="option4"><label for="option4" class="option"> a right-angled triangle</label>
+				  <div class="option"><input type="radio" name="answer" value="1" class="option" id="option1"><label for="option1" class="option"></label></div>
+				  <div class="option"><input type="radio" name="answer" value="2" class="option" id="option2"><label for="option2" class="option"></label></div>
+				  <div class="option"><input type="radio" name="answer" value="3" class="option" id="option3"><label for="option3" class="option"></label></div>
+				  <div class="option"><input type="radio" name="answer" value="4" class="option" id="option4"><label for="option4" class="option"></label></div>
 				</form>
 			</div>
 		</div>
 		<div class="row" id="bottom-bar">
-			<div class="col-md-5 pull-right text-right">
+			<div id="warning" class="col-md-4">
+				<p>Select an option!</p>
+			</div>
+			<div class="col-md-8 pull-right text-right">
 				<p id="correct">Correct: 0</p>
 				<p id="wrong">Wrong: 0</p>
 				<button id="next" onclick="next()">Next</button>
 			</div>
 		</div>
 	</div>
-
-
 </body>
 </html>
--- multiple_choice.js
+++ multiple_choice.js
@@ -14,9 +14,12 @@ var jsonData;
 var mute = false;
 var startTime;
 var endTime;
+var firstClick;
 
 // Sounds
 var clickSound;
+var correctSound;
+var wrongSound;
 
 // HTML elements we will be updating.
 var questionTextElement;
@@ -34,28 +37,51 @@ var wrongTextElement;
 function init(title) {
   versionName = title;
 
+  // Show the start screen.
+  showStartScreen();
+
   // Load sounds.
   clickSound = new Audio('sounds/click.mp3');
+  correctSound = new Audio('sounds/correct.mp3');
+  wrongSound = new Audio('sounds/wrong.mp3');
 
-  // Load elements.
+  // Initialize arrays to hold some HTML elements.
   optionLabelElements = [];
   optionInputElements = [];
 
+  // Load array of <label>'s.
   $("label.option").each(function() {
     optionLabelElements.push(this)
   });
 
+  // Load array of <input>'s.
   $("input.option").each(function() {
     optionInputElements.push(this)
   });
 
-
-
+  // Load some other HTML <p> and <img> elements into variables.
   questionTextElement = document.getElementById("question-text");
   questionImageElement = document.getElementById("question-image");
   correctTextElement = document.getElementById("correct");
   wrongTextElement = document.getElementById("wrong");
 
+  // Make the option <div>'s clickable.
+  $("div.option").each(function() {
+    this.onclick = function() {
+      clearSelectedDiv();
+      this.childNodes[0].checked = true; // Check the radio button.
+      this.classList.add("checked"); // Highlight the <div>.
+    }
+  });
+
+  // Add listener to radio buttons. Highlight parent <div> when they are clicked.
+  $('input[type="radio"]').click(function(){
+    clearSelectedDiv();
+    if ($(this).is(':checked'))
+    {
+      this.parentElement.classList.add("checked");
+    }
+   });
 
   // Load the version JSON data into our variable.
   $.getJSON("versions/" + versionName + "/" + versionName + ".json", function(json) {
@@ -64,12 +90,12 @@ function init(title) {
     // Set title.
     document.getElementById("game-title").innerHTML = jsonData.title;
 
-    // Load the first question.
+    // Reset counters and variables. This is needed when game is reset.
     counter = correctCounter = wrongCounter = 0;
-    loadQuestion();
+    firstClick = true;
 
-    // Start timer.
-    startTime = new Date().getTime();
+    // Load the first question.
+    loadQuestion();
   });
 }
 
@@ -78,24 +104,70 @@ function init(title) {
  */
 function next() {
 
-  // Check that at least one radio button is checked.
-  if ($("input[type=radio]:checked").length > 0) {
-    // Check if correct.
-    let correct = false;
-    for (var radio of optionInputElements) {
-      if (radio.checked) {
+  // Check if this is the first or second time the user has clicked 'Next'.
+  // On the first click, show the correct answer.
+  // On the second click, move to the next question.
+  if (firstClick) {
+    // Check that at least one radio button is checked.
+    if ($("input[type=radio]:checked").length > 0) {
+
+      // Check if correct.
+      let correct = false;
+      // For each radio button...
+      for (var radio of optionInputElements) {
+
+        // If this radio button is the correct answer.
         if (radio.value === jsonData.questions[counter].answer) {
-          correct = true;
+          // Highlight it.
+          radio.parentElement.classList.add("correct");
+          // If the correct radio button is checked, the user has chosen the correct answer.
+          if (radio.checked) {
+            correct = true;
+          }
+        // If this is not the correct radio button, but it is checked.
+        } else if (radio.checked) {
+          // The user has chosen the wrong option.
+          radio.parentElement.classList.add("wrong");
         }
       }
-    }
 
-    // Notify user if selection was correct/incorrect.
-    if (correct) {
-      correctTextElement.innerHTML = "Correct: " + ++correctCounter;
+      // Notify user if selection was correct/incorrect.
+      if (correct) {
+        // Play the 'correct' sound.
+        playSound(correctSound);
+        // Increment the 'correct' counter.
+        correctTextElement.innerHTML = "Correct: " + ++correctCounter;
+      } else {
+        // Play the 'wrong' sound.
+        playSound(wrongSound);
+        // Increment the 'wrong' counter.
+        wrongTextElement.innerHTML = "Wrong: " + ++wrongCounter;
+      }
+
+      // The next click will be the second click.
+      firstClick = false;
+
+    // An option was not selected.
     } else {
-      wrongTextElement.innerHTML = "Wrong: " + ++wrongCounter;
+      // Notify the user that an option must be selected.
+      $("#warning").css("display", "block");
+      setTimeout(function() {
+        $("#warning").css("display", "none");
+      }, 2000);
+
     }
+  } else {
+
+    // Clear CSS class on option <div>.
+    $('input[type="radio"]').each(function() {
+      this.parentElement.classList.remove("checked");
+    });
+
+    // Reset correct/incorrect highlights.
+    firstClick = true;
+    $("div.option").each(function() {
+      $(this).removeClass("wrong correct");
+    })
 
     // Play a click sound.
     playSound(clickSound);
@@ -104,17 +176,23 @@ function next() {
     counter++;
     if (counter < jsonData.questions.length) {
       loadQuestion();
+      // If the next question is the last question.
       if (counter === jsonData.questions.length - 1) {
+        // Change the 'Next' button to read 'Finish'.
         document.getElementById("next").innerHTML = "Finish";
       }
     } else {
+      // Call a function which displays the end game screen.
       endGame();
     }
-  } else {
-    alert("Select an option.");
   }
+
 }
 
+/*
+ * Show the end game <div>.
+ * Hide the options <div>.
+ */
 function endGame() {
   $("#question-row").css("display", "none");
   $("#game-over-row").css("display", "flex");
@@ -125,17 +203,25 @@ function endGame() {
   $("#total-time").html("Total Time: " + ((new Date().getTime() - startTime)/1000).toFixed(1) + " seconds");
 }
 
+/*
+ * Called when the 'Restart' button is clicked.
+ * Undo settings done by endGame and call the init function.
+ */
 function restart() {
-  $("#question-row").css("display", "inline");
+  playSound(clickSound);
+  $("#question-row").css("display", "block");
   $("#game-over-row").css("display", "none");
   $("form").css("visibility", "visible");
   $("#next").html("Next");
   $("#wrong").html("Wrong: 0");
   $("#correct").html("Correct: 0");
-    $("#next").prop("disabled", false);
+  $("#next").prop("disabled", false);
   init(versionName);
 }
 
+/*
+ * Load a question based on the value of the variable 'counter'.
+ */
 function loadQuestion() {
   // Access question from JSON data.
   let question = jsonData.questions[counter];
@@ -153,4 +239,42 @@ function loadQuestion() {
     optionLabelElements[i].innerHTML = question.options[i].option;
     optionInputElements[i].value = question.options[i].option;
   }
+}
+
+/*
+ * Hide main quiz <div>'s.
+ * Show the start screen <div>.
+ */
+function showStartScreen() {
+  $("#question-row").css("display", "none");
+  $("form").css("visibility", "hidden");
+  $("#next").prop("disabled", true);
+  $("#start-row").css("display", "flex");
+}
+
+/*
+ * Called when start button is pressed.
+ * Removes start screen and shows main quiz <div>'s.
+ */
+function start() {
+  playSound(clickSound);
+  $("#question-row").css("display", "block");
+  $("#start-row").css("display", "none");
+  $("form").css("visibility", "visible");
+  $("#next").html("Next");
+  $("#wrong").html("Wrong: 0");
+  $("#correct").html("Correct: 0");
+  $("#next").prop("disabled", false);
+
+  // Start timer.
+  startTime = new Date().getTime();
+}
+
+/*
+ * Remove 'checked' class from all option <div>'s.
+ */
+function clearSelectedDiv() {
+  $('input[type="radio"]').each(function() {
+    this.parentElement.classList.remove("checked");
+  });
 }
